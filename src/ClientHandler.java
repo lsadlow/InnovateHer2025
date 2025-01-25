fea34553591fa23d82b69bbf8507dec4307ac8b2
Your Name <budugu@pal-nat186-8-111.itap.purdue.edu>
2025-01-25 18:54:00 -0500

    basic server-client

---
+++ src/ClientHandler.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.net.*;
+
+// ClientHandler class
+class ClientHandler extends Thread
+{
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    final Socket s;
+
+    public static void main(String[] args) {
+
+    }
+
+    // Constructor
+    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos)
+    {
+        this.s = s;
+        this.dis = dis;
+        this.dos = dos;
+    }
+
+    @Override
+    public void run()
+    {
+        String received;
+        String toreturn;
+        while (true)
+        {
+            try {
+                // Ask user what he wants
+                dos.writeUTF("client handler working");
+
+                // receive the answer from client
+                received = dis.readUTF();
+
+                if(received.equals("Exit"))
+                {
+                    System.out.println("Client " + this.s + " sends exit...");
+                    System.out.println("Closing this connection.");
+                    this.s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try
+        {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
