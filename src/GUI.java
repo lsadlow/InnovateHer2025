a3e4d5f33657b7c4d0be6597da9dfcb04ddf3d8d
Laila Sadlow <lsadlow@purdue.edu>
2025-01-25 11:30:38 -0500

    Added placeholder welcome message

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,15 @@
 import javax.swing.*;
 
 public class GUI {
+    public GUI() {
+
+    }
+
+    public static void displayWelcomeMessage() {
+        JOptionPane.showMessageDialog(null, "Welcome to our app!");
+    }
+
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
 }
